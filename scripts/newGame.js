2c3b44aa8ca15c3b8a4f67409f75f2a5773bac5e
RadkaPav <153281974+RadkaPav@users.noreply.github.com>
2024-03-25 13:57:18 +0100

    Update newGame.js

--- scripts/newGame.js
+++ scripts/newGame.js
@@ -1,7 +1,6 @@
 import { data } from "./data.js"
 import { container, goalContainer, img } from "./querySelectors.js"
 
-// Náhodné zamíchání písmen
 function shuffleLetters(letters) {
     for (let i = 0; i < letters.length; i++) {
         let number = Math.floor(Math.random() * (letters.length - i))
@@ -17,13 +16,13 @@ export const newGame = (index) => {
     const letters = data[index].letters
 
     shuffleLetters(letters)
-    //pokud jsou písmena v původním pořadí, míchání se spustí znovu
+ 
     const oldOrderLetters = data[index].word
     const newOrderLetters = letters.join("").toLowerCase()
     if (newOrderLetters === oldOrderLetters) {
         shuffleLetters(letters)
     }
-    //vytvoření políček s písmeny
+    
     for (let j = 0; j < data[index].letters.length; j++) {
         const div = document.createElement("div")
         div.classList.add("origin", "div")
@@ -34,7 +33,7 @@ export const newGame = (index) => {
         for (let k = 0; k < boxes.length; k++) {
             boxes[k].setAttribute("id", `${k}`)
         }
-        //vytvoření cílových políček
+       
         const goalDiv = document.createElement("div")
         goalDiv.classList.add("goal", "div")
         goalContainer.appendChild(goalDiv)
