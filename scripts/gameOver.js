77310d12bad72e3f82b38f81fbd70fe7151457d1
RadkaPav <153281974+RadkaPav@users.noreply.github.com>
2024-03-25 13:56:41 +0100

    Update gameOver.js

--- scripts/gameOver.js
+++ scripts/gameOver.js
@@ -2,19 +2,18 @@ import { data } from "./data.js"
 import { index } from "./buttons.js"
 import { button } from "./querySelectors.js"
 
-//KONEC HRY
 export const gameOver = () => {
  const draggedBoxes = document.querySelectorAll(".dragged")
     let results = []
     const correctAnswer = data[index].word
 
-    draggedBoxes.forEach((box) => {     //přetažená písmena přidá do results
+    draggedBoxes.forEach((box) => {    
         results.push(box.innerText)
     })
 
     const result = results.join("").toLowerCase()
     
-    if (result === correctAnswer) {         //při správné odpovědi se zobrazí tlačítko pro posun na další slovo
+    if (result === correctAnswer) {        
         button.classList.remove("invisible")
     }
 
